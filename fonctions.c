07797d0ea67690fce382f8055286698a94a3cfce
bohbot <vincbob@free.fr>
2013-09-28 17:51:10 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -1,53 +1,135 @@
-
 #include <stdio.h>
-#include <tchar.h>
-#include "fonction.h"
+#include "fonctions.h"
+#include <time.h>
 
-void affiche_film( film* ptr_film )
+void affiche_genre(int genre)
 {
-	film film1=*ptr_film;
-	printf("Fiche FILM\n");
-	printf("Titre : %s (%d)\n",film1.titre,film1.annee);
-	printf("realisateur : %s\n",film1.nom_realisateur);
-	printf("Acteurs : %s\n",film1.nom_acteur);
-	printf("Duree : %d\n",film1.duree);
-	printf("Genre : %s\n",film1.genre1);
+	switch( genre )
+	{
+		case 0:
+			printf("Action");
+		break;
+		case 1:
+			printf("Horreur");
+		break;
+		case 2:
+			printf("Comedie");
+		break;
+		case 3:
+			printf("Documentaire");
+		break;
+		case 4:
+			printf("Policier");
+		break;
+		case 5:
+			printf("Drame");
+		break;
+		case 6:
+			printf("Animation");
+		break;
+		case 7:
+			printf("Science Fiction");
+		break;
+	}
 }
 
-void affiche_acteur( acteur* ptr_acteur )
+void affiche_film( Film* ptr_film )
 {
-	acteur acteur1=*ptr_acteur;
-	printf("Fiche Acteur\n");
+	Film film1=*ptr_film;
+	printf("Fiche FILM\n");
+	printf("Titre : %s (%d)\n",film1.titre,film1.annee);
+	printf("Realisateur : %s\n",film1.nom_realisateur);
+	printf("Acteurs : %s",film1.nom_acteur1);
+	if (film1.nom_acteur2 != "")
+		printf(", %s",film1.nom_acteur2);
+	if (film1.nom_acteur3 != "")
+		printf(", %s",film1.nom_acteur3);
+	if (film1.nom_acteur4 != "")
+		printf(", %s",film1.nom_acteur4);
+	printf("\n");
+	printf("Duree : %d min\n",film1.duree);
+	printf("Genre : ");
+	affiche_genre(film1.genre1);
+	printf("\n");
+}
+
+void affiche_acteur( Acteur* ptr_acteur )
+{
+	Acteur acteur1=*ptr_acteur;
+	printf("Fiche ACTEUR\n");
 	printf("Nom : %s %s\n",acteur1.nom, acteur1.prenom);
-	printf("Date de naissance : %d\n",acteur1.date_naissance);
+	printf("Date de naissance : %d-%s-%d\n",acteur1.jour_naissance, acteur1.mois_naissance, acteur1.annee_naissance);
 	printf("Nationalite : %s\n",acteur1.nationalite);
 }
 
-void affiche_realisateur( realisateur* ptr_realisateur )
+void affiche_realisateur( Realisateur* ptr_realisateur )
 {
-	realisateur realisateur1=*ptr_realisateur;
-	printf("Fiche Realisateur\n");
+	Realisateur realisateur1=*ptr_realisateur;
+	printf("Fiche REALISATEUR\n");
 	printf("Nom : %s %s\n",realisateur1.nom, realisateur1.prenom);
-	printf("Date de naissance : %d\n",realisateur1.date_naissance);
+	printf("Date de naissance : %d-%s-%d\n",realisateur1.jour_naissance, realisateur1.mois_naissance, realisateur1.annee_naissance);
 	printf("Nationalite : %s\n",realisateur1.nationalite);
 }
 
 
-void recherche_film( film table_film[], int taille_tab, char nom_film )
+void recherche_film( Film *table_film, int taille_tab, char *titre_film )
 {
-    int i;
-	for (i=0;i<taille_tab;i++)
+	char genre[60];
+	for ( int i=0; i<taille_tab; i++ )
 	{
-		if ( table_film[i].titre == nom_film )
+		if ( !strcmp(table_film[i].titre, titre_film) )
 		{
-		    film film1=table_film[i];
 			printf("Fiche FILM\n");
-			printf("Titre : %s (%d)\n",film1.titre,film1.annee);
-			printf("realisateur : %s\n",film1.nom_realisateur);
-			printf("Acteurs : %s\n",film1.nom_acteur);
-			printf("Duree : %d\n",film1.duree);
-			printf("Genre : %s\n",film1.genre1);
+			printf("Titre : %s \n",table_film[i].titre);
+			printf("Realisateur : %s\n",table_film[i].nom_realisateur);
+			printf("Acteurs : %s\n",table_film[i].nom_acteur1);
+			if (table_film[i].nom_acteur2 != "")
+				printf(", %s",table_film[i].nom_acteur2);
+			if (table_film[i].nom_acteur3 != "")
+				printf(", %s",table_film[i].nom_acteur3);
+			if (table_film[i].nom_acteur4 != "")
+				printf(", %s",table_film[i].nom_acteur4);
+			printf("\n");
+			printf("Duree : %d\n",table_film[i].duree);
+			printf("Genre : ");
+			affiche_genre(table_film[i].genre1);
+			printf("\n");
 		}
 	}
-	return;
+}
+
+void affiche_filmographie_acteur( Film *table_film, int taille_tab, char *nom_acteur )
+{
+	printf("Resultats : %s\n", nom_acteur);
+	for ( int i=0; i < taille_tab; i++ )
+	{
+		if ( !strcmp(table_film[i].nom_acteur1, nom_acteur) )
+		{
+			printf( "%s (%d) \n", table_film[i].titre, table_film[i].annee );
+		}
+		else if ( !strcmp(table_film[i].nom_acteur2, nom_acteur) )
+		{
+			printf( "%s (%d) \n", table_film[i].titre, table_film[i].annee );
+		}		
+		else if ( !strcmp(table_film[i].nom_acteur3, nom_acteur) )
+		{
+			printf( "%s (%d) \n", table_film[i].titre, table_film[i].annee );
+		}
+		else if ( !strcmp(table_film[i].nom_acteur4, nom_acteur) )
+		{
+			printf( "%s (%d) \n", table_film[i].titre, table_film[i].annee );
+		}
+	}
+}
+
+void affiche_filmographie_realisateur( Film *table_film, int taille_tab, char *nom_realisateur )
+{
+	printf("Resultats : %s\n", nom_realisateur);
+	for ( int i=0; i < taille_tab; i++ )
+	{
+		if ( !strcmp(table_film[i].nom_realisateur, nom_realisateur) )
+		{
+			printf( "%s (%d) \n", table_film[i].titre, table_film[i].annee );
+		}
+	}
 }
